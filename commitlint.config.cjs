aeaccfa781514a98be092f3ba924798a3178f920
nabi.lee <hello@nabi.kim>
2023-11-10 21:03:35 +0900

    chore(repo): set scope to exist packages only

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,5 +1,24 @@
+const { readdirSync } = require('fs');
+const { join } = require('path');
+
 const Configuration = {
   extends: ['@commitlint/config-conventional'],
+  utils: { getPackages },
+  rules: {
+    'scope-enum': (ctx) => getPackages(ctx).then((packages) => [2, 'always', packages]),
+  },
 };
+
+async function getPackages(context) {
+  return Promise.resolve().then(() => {
+    const ctx = context || {};
+    const cwd = ctx.cwd || process.cwd();
+
+    const appProjects = readdirSync(join(cwd, 'apps'));
+    const packageProjects = readdirSync(join(cwd, 'packages'));
+
+    return ['repo', ...appProjects, ...packageProjects];
+  });
+}
 
 module.exports = Configuration;
